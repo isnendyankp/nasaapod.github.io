2514f63acadc6d678383692e1773e0290049ff19
isnendyan <isnendyankp@gmail.com>
2023-05-16 07:54:44 +0700

    JS-Create DOM Elements: create const on js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
 
 // NASA API
 const count = 10;
