e643ab6c396c37c29fbbdfa12055558944b97160
isnendyan <isnendyankp@gmail.com>
2023-05-17 07:23:10 +0700

    JS-Create DOM Elements: fix save text with display on console.log

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function updateDOM() {
     const saveText = document.createElement('p');
     saveText.classList.add('clickable');
     saveText.textContent = 'Add To Favorites';
-    saveText.onclick = `saveFavorite('${result.url}')`;
+    saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
     // Card Text
     const cardText = document.createElement('p');
     cardText.textContent = result.explanation;
